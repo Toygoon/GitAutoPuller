f713c8fa7212801b5da4c3c6609e771c97b82644
Toygoon <toygoon@gmail.com>
2022-09-20 00:29:20 +0900

    Initial commit

---
+++ src/main/java/com/toygoon/gitautopuller/GitAutoPullerApplication.java
@@ -0,0 +1,13 @@
+package com.toygoon.gitautopuller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitAutoPullerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitAutoPullerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toygoon/gitautopuller/GitAutoPullerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toygoon.gitautopuller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitAutoPullerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
