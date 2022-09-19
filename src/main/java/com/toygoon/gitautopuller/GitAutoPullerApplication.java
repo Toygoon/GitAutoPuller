dccf18b0ff95de2e317eae6f625c6a30b0fbe68e
Toygoon <toygoon@gmail.com>
2022-09-20 00:41:13 +0900

    Edit: Misc

--- src/main/java/com/toygoon/gitautopuller/GitAutoPullerApplication.java
+++ src/main/java/com/toygoon/gitautopuller/GitAutoPullerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitAutoPullerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GitAutoPullerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GitAutoPullerApplication.class, args);
+    }
 
 }
--- src/test/java/com/toygoon/gitautopuller/GitAutoPullerApplicationTests.java
+++ src/test/java/com/toygoon/gitautopuller/GitAutoPullerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GitAutoPullerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
