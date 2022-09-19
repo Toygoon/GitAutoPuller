9ed168aeabf7c23cb2085d73fa6fab6d9b9837c0
Toygoon <toygoon@gmail.com>
2022-09-20 00:51:43 +0900

    test commit 1

--- src/main/java/com/toygoon/gitautopuller/GitAutoPullerApplication.java
+++ src/main/java/com/toygoon/gitautopuller/GitAutoPullerApplication.java
@@ -11,6 +11,6 @@ public class GitAutoPullerApplication {
         Configuration config = new Configuration();
         config.readConfig();
 
-        SpringApplication.run(GitAutoPullerApplication.class, args);
+        SpringApplication.run(GitAutoPullerApplication.class, args);1
     }
 }
